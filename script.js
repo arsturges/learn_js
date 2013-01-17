37ff49f473cdadcffb0f907333abebd55daf0942
Andy Sturges <arsturges@lbl.gov>
2013-01-16 22:24:11 -0800

    insertAfter(), remove(), text(), html()

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+  $('#no-script').remove();
   $('h1 + p').addClass('large_blue');
-
+  $('<input type="button" id="toggleButton" value="toggle" />')
+    .insertAfter('#disclaimer');
   $('#toggleButton').click(function() {
     $('#disclaimer').toggle();
     if ($('#disclaimer').is(':visible')) {
@@ -9,5 +11,7 @@ $(document).ready(function() {
       $(this).val('Show'); 
     }
   });
+  $('p').html('<strong>Goodbye</strong>, cruel paragraphs!');
+  $('h2').text('<strong>All your h2 are belong to us!</strong>');
 });
 
